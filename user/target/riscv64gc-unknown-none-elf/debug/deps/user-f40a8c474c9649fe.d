3ebfb3f7b765346214d9cb8ac180c70007641db5
knifefire <2113130664@qq.com>
2024-03-29 17:47:31 +0800

    适配K210开发板

---
+++ user/target/riscv64gc-unknown-none-elf/debug/deps/user-f40a8c474c9649fe.d
@@ -0,0 +1,7 @@
+/home/knifefire/RustroverProjects/MyOs/user/target/riscv64gc-unknown-none-elf/debug/deps/libuser-f40a8c474c9649fe.rmeta: src/lib.rs
+
+/home/knifefire/RustroverProjects/MyOs/user/target/riscv64gc-unknown-none-elf/debug/deps/libuser-f40a8c474c9649fe.rlib: src/lib.rs
+
+/home/knifefire/RustroverProjects/MyOs/user/target/riscv64gc-unknown-none-elf/debug/deps/user-f40a8c474c9649fe.d: src/lib.rs
+
+src/lib.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/debug/libuser.d
@@ -0,0 +1,1 @@
+/home/knifefire/RustroverProjects/MyOs/user/target/riscv64gc-unknown-none-elf/debug/libuser.rlib: /home/knifefire/RustroverProjects/MyOs/user/src/lib.rs
